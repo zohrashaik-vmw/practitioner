03dbe8830186d74e778279ab0b8b9cefbd9f949e
zohrashaik-vmw <71278998+zohrashaik-vmw@users.noreply.github.com>
2021-09-30 20:50:42 -0500

    changed  Strategy to Identity for the Practitioner entity

--- src/main/java/com/livewell/practitioner/model/Practitioner.java
+++ src/main/java/com/livewell/practitioner/model/Practitioner.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Practitioner {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String firstName;
     private String lastName;
